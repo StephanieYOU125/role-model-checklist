144b88da6293c72d06bd83efe5730a713ce704bf
Wun-Sin You <152066727+StephanieYOU125@users.noreply.github.com>
2026-09-14 21:25:00 +0800

    Refresh iPhone home screen icon cache

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='role-model-checklist-v2';
-const ASSETS=['./','./index.html','./manifest.json','./icon.svg'];
+const CACHE='role-model-checklist-v3';
+const ASSETS=['./','./index.html','./manifest.json?v=3','./icon.svg?v=3','./apple-touch-icon.svg?v=3'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS))));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
 self.addEventListener('fetch',event=>event.respondWith(caches.match(event.request).then(cached=>cached||fetch(event.request))));
